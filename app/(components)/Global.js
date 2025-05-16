33592625e0bf5e40847be2ca7135466cb9c5f07e
AccfsciIT <ram.babu@accf.in>
2025-05-16 10:38:00 +0530

    '160425'

---
+++ app/(components)/Global.js
@@ -0,0 +1,22 @@
+export const color1 = '#bf1e2e'; //reddish brown
+export const color2 = '#f7941d';
+export const color3 = '#1976d2'; //default blue
+export const color4 = '#ff7b00'; // orange
+export const color5 = '#bd150b'; //red
+
+export const color6 = '#9d50bb'; //
+export const color7 = '#6e48aa'; //
+
+export const MedantaOrange = '#f04e30';
+
+export const Color8 = 'white';
+export const Color9 = 'black'
+export const NavBackground = null;
+export const NavElements = Color8;
+
+export const color = color3;
+export const HomeDoctorList = color1;
+export const DoctorDetails = color1;
+export const ContactColor = MedantaOrange;
+export const Font = 'montserrat';
+export const HName = 'SCI/';
